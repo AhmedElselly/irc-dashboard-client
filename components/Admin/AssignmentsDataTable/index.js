f93c8f6f2d5565189c86a804b853d05cf87a1d46
ahmedelselly <ahmedelselly@localhost.localdomain>
2022-05-17 11:26:14 +0200

    added datatable for assignments users

---
+++ components/Admin/AssignmentsDataTable/index.js
@@ -0,0 +1,493 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import Link from 'next/link';
+import {getStudents, isAuthenticated, getSchools, getUsers, removeUser} from '../../../actions/userApi';
+import {getPosts} from '../../../actions/postApi';
+
+import {useRouter} from 'next/router';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    _id: 'tracking id',
+    numeric: false,
+    disablePadding: true,
+    label: 'Tracking ID',
+  },
+  {
+    _id: 'name',
+    numeric: true,
+    disablePadding: false,
+    label: 'Name',
+  },
+  {
+    _id: 'email',
+    numeric: true,
+    disablePadding: false,
+    label: 'Email',
+  },
+  {
+    _id: 'birth',
+    numeric: true,
+    disablePadding: false,
+    label: 'Birth',
+  },
+  {
+    _id: 'role',
+    numeric: true,
+    disablePadding: false,
+    label: 'Role',
+  },
+  {
+    _id: 'actions',
+    numeric: true,
+    disablePadding: false,
+    label: 'Actions',
+  }
+];
+
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+    props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell._id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell._id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell._id}
+              direction={orderBy === headCell._id ? order : 'asc'}
+              onClick={createSortHandler(headCell._id)}
+            >
+              {headCell.label}
+              {orderBy === headCell._id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+  const { numSelected, title, rows, onDelete} = props;
+
+  const handleRemove = (num) => {
+    console.log('delete num', num)
+    console.log('delete', rows[num])
+    onDelete(rows[num]);
+  }
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          {title}
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton onClick={() => handleRemove(numSelected)}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+export default function EnhancedTable({title}) {
+  const router = useRouter();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = React.useState([]);
+  const [toRemove, setToRemove] = React.useState([]);
+
+  const [show, setShow] = React.useState(false);
+  const [user, setUserId] = React.useState('');
+
+  const getAllUsers = () => {
+    // console.log(router.pathname)
+    if(router.pathname === '/assignments'){
+      getPosts().then(res => {
+        console.log(res.data);
+        setRows(res.data);
+      })
+    } 
+    
+    
+  };
+
+  React.useEffect(() => {
+    getAllUsers();
+    console.log('rows', rows)
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n._id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const handleRemove = (_id) => {
+    setShow(true);
+    setUserId(_id);
+  }
+
+
+  const handleClickOpenDial = () => {
+    setShow(true);
+  };
+
+  const handleCloseDial = (answer) => {
+    // setShow(false);
+    if(answer === 'yes'){
+      const userId = isAuthenticated().user._id;
+      let newArr;
+      console.log('user and answer', answer, user);
+      removeUser(user, userId).then(res => {
+        console.log(res.data);
+        newArr = rows.filter(person => person._id !== user);
+        setRows(newArr);
+        setShow(false);
+      })
+    } else {
+      setShow(false);
+      setUserId('');
+    }
+    
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        {/* <EnhancedTableToolbar onDelete={handleRemove} rows={rows} title={title} numSelected={selected.length} /> */}
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row._id);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      
+                      <TableCell onClick={(event) => handleClick(event, row._id)} padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId,
+                          }}
+                        />
+                      </TableCell>
+                      
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row._id}
+                      </TableCell>
+                      
+                      <TableCell align="right">{row.name}</TableCell>
+                      <TableCell align="right">{row.email}</TableCell>
+                      <TableCell align="right">{row.dateOfBirth}</TableCell>
+                      {row.admin && <TableCell align="right">{row.admin && ('Admin')}</TableCell>}
+                      {row.student && <TableCell align="right">{row.student && ('Student')}</TableCell>}
+                      {row.school && <TableCell align="right">{row.school && ('School')}</TableCell>}
+                      <TableCell align="right">
+                       <Link href={`/assignments/${row._id}`}>
+                          <a style={{
+                            background: '#fff',
+                            color: 'blueviolet',
+                            border: '1px solid blueviolet',
+                            paddingLeft: 15,
+                            paddingRight: 15,
+                            paddingTop: 5,
+                            paddingBottom: 5,
+                            borderRadius: 5
+                          }}>View</a>
+                        </Link>
+                       {/* {isAuthenticated() && isAuthenticated().user.admin && router.pathname !== '/assignments' && <Link href={`/user/${row._id}/edit`}>
+                          <a style={{
+                            background: '#fff',
+                            color: 'green',
+                            border: '1px solid green',
+                            paddingLeft: 15,
+                            paddingRight: 15,
+                            paddingTop: 5,
+                            paddingBottom: 5,
+                            borderRadius: 5,
+                            marginLeft: 5
+                          }}>Edit</a>
+                        </Link>}
+                       {isAuthenticated() && isAuthenticated().user.admin && router.pathname !== '/assignments' && <button style={{
+                            background: '#fff',
+                            color: '#D01B59',
+                            border: '1px solid #D01B59',
+                            paddingLeft: 15,
+                            paddingRight: 15,
+                            paddingTop: 5,
+                            paddingBottom: 5,
+                            borderRadius: 5,
+                            marginLeft: 5,
+                            cursor: 'pointer'
+                          }}
+                          onClick={() => handleRemove(row._id)}
+                          >
+                          Remove
+                        </button>} */}
+                      </TableCell>
+                      
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      {/* <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      /> */}
+      <Dialog
+        open={show}
+        keepMounted
+        onClose={handleCloseDial}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Confirm remove"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Are you sure you want to remove the user?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleCloseDial('yes')}>Yes</Button>
+          <Button onClick={() => handleCloseDial('no')}>No</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+}
--- pages/assignments/index.js
+++ pages/assignments/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../../styles/Post.module.css';
 import Sidebar from '../../components/Admin/Sidebar';
+import AssignmentsDataTable from '../../components/Admin/AssignmentsDataTable';
 
 const url = 'https://ircbackend.herokuapp.com/api/users'
 // const url = 'http://localhost:8000/api/users'
@@ -17,22 +18,22 @@ const Assignments = props => {
 		})
 	}, []);
 
-	const generatePosts = () => {
-		return users?.map(user => {
-			console.log(user._id)
-			return (
-				<Link href={`/assignments/${user._id}`} passHref>
-					<a>
-						<div className={styles.user}>
-							{user.image && <Image className={styles.image} width={100} height={100} src={`${url}/user/image/${user._id}`} />}
-							{!user.image && <Image className={styles.image} width={100} height={100} src='/user.png' />}
-							<h4 className={styles.username}>{user && user.name}</h4>
-						</div>
-					</a>
-				</Link>
-			)
-		})
-	}
+	// const generatePosts = () => {
+	// 	return users?.map(user => {
+	// 		console.log(user._id)
+	// 		return (
+	// 			<Link href={`/assignments/${user._id}`} passHref>
+	// 				<a>
+	// 					<div className={styles.user}>
+	// 						{user.image && <Image className={styles.image} width={100} height={100} src={`${url}/user/image/${user._id}`} />}
+	// 						{!user.image && <Image className={styles.image} width={100} height={100} src='/user.png' />}
+	// 						<h4 className={styles.username}>{user && user.name}</h4>
+	// 					</div>
+	// 				</a>
+	// 			</Link>
+	// 		)
+	// 	})
+	// }
 	return (
 		<div className={styles.container}>
 			<div className={styles.left}>
@@ -43,7 +44,8 @@ const Assignments = props => {
 					<h1 className={styles.header}>Assignments</h1>
 				</div>
 				<div className={styles.wrapper}>
-					{generatePosts()}
+					{/* {generatePosts()} */}
+					<AssignmentsDataTable />
 				</div>
 			</div>
 		</div>
