0c482dc912903c5fcbd36c3371a0ad5f0da64c85
ahmedelselly <ahmedelselly@localhost.localdomain>
2022-05-19 17:28:32 +0200

    adjusted the port

--- actions/messageApi.js
+++ actions/messageApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const url = 'https://ircbackend.herokuapp.com/api/messages';
 // const url = 'http://localhost:8000/api/messages';
-const url = '64.227.74.120/api/messages';
+const url = '64.227.74.120:8000/api/messages';
 
 export const index = async () => {
     const res = await axios.get(`${url}`);
